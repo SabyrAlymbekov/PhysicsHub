2fbde663553cfe1e3754b3a654ad8cb64610faee
Sabyr Alymbekov <alymbekovsabyr@gmail.com>
2024-11-05 03:42:27 +0600

    fixed filters

--- components/shared/olympiads/RenderOlympiads.tsx
+++ components/shared/olympiads/RenderOlympiads.tsx
@@ -10,33 +10,31 @@ interface RenderOlympiadsProps {
     showUpcoming: boolean;
 }
 
-const RenderOlympiads: React.FC<RenderOlympiadsProps> = ({ olympiads, dateRange, showUpcoming }: RenderOlympiadsProps) => {
+const RenderOlympiads: React.FC<RenderOlympiadsProps> = ({ olympiads, dateRange, showUpcoming }) => {
     const now = new Date();
 
     const filteredOlympiads = olympiads.filter((olympiad) => {
         const registrationStart = olympiad.registrationStart ? new Date(olympiad.registrationStart) : null;
-        const d = olympiad.registrationEnd ? new Date(olympiad.registrationEnd) : null;
+        const registrationEnd = olympiad.registrationEnd ? new Date(olympiad.registrationEnd) : null;
 
         // Фильтр по диапазону дат
-        const isInDateRange = dateRange && olympiads.some(olympiad => {
-            const { registrationStart, registrationEnd } = olympiad;
-            return registrationStart && registrationEnd && dateRange.from && dateRange.to
-                ? isWithinInterval(registrationStart, { start: dateRange.from, end: dateRange.to }) ||
-                  isWithinInterval(d as Date, { start: dateRange.from, end: dateRange.to })
-                : true;
-        });
+        const isInDateRange = dateRange && dateRange.from && dateRange.to
+            ? (registrationStart && isWithinInterval(registrationStart, { start: dateRange.from, end: dateRange.to })) ||
+              (registrationEnd && isWithinInterval(registrationEnd, { start: dateRange.from, end: dateRange.to }))
+            : true;
 
+        // Фильтр по предстоящим олимпиадам
         const isUpcoming = showUpcoming ? registrationStart && isAfter(registrationStart, now) : true;
 
         return isInDateRange && isUpcoming;
     });
 
     return (
-      <div className="flex flex-col gap-5 mb-20">
-          {filteredOlympiads.map((olympiad) => (
-            <OlympiadBlock olympiad={olympiad} key={olympiad.id} />
-          ))}
-      </div>
+        <div className="flex flex-col gap-5 mb-20">
+            {filteredOlympiads.map((olympiad) => (
+                <OlympiadBlock olympiad={olympiad} key={olympiad.id} />
+            ))}
+        </div>
     );
 };
 
