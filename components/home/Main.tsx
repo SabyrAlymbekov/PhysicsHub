39cf8e89a1d514315eaefca26d797d1d6aed31f5
Nursul-stack <oscilresponse@gmail.com>
2024-11-06 17:47:06 +0600

    Update Main.tsx

--- components/home/Main.tsx
+++ components/home/Main.tsx
@@ -5,7 +5,7 @@ function Main() {
   return (
     <section className='bg-gray-100 w-full py-5 md:py-24'>
         <div className='container py-10 flex flex-col justify-center gap-5'>
-            <h1 className='title'>Physics Hub</h1>
+            <h1 className='title'>Попращайтесь с проблемами в изучении <span className='text-gradient'>физики</span></h1>
             <p className='subtitle'>Physics Hub – это платформа для изучения физики.
                 <br/>
                 Наша цель сделать изучение физики <span className='text-gradient'>доступным для всех</span>!
