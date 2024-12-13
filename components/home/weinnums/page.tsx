dbb33a1e8e1e1865ca848aed8778efc55abb474e
Nursul-stack <oscilresponse@gmail.com>
2024-12-14 04:50:09 +0600

    Update page.tsx

--- components/home/weinnums/page.tsx
+++ components/home/weinnums/page.tsx
@@ -3,7 +3,7 @@ const WeInNums = () => {
     <div className="py-20 w-full bg-gray-100">
         <div className="container flex justify-between flex-col gap-8">
           <h1 className="font-bold text-4xl md:text-5xl 2xl:text-6xl">
-            Комюнити из тысяч людей
+            Сообщество из тысяч людей
           </h1>
           <p className="subtitle !text-left max-w-[1200px]">
             Physics Hub - международное сообщество для любителей науки, основанное Нурсултаном Раяповым, <span className="text-gradient">призёром олимпиад и членом сборной Кыргызстана по физике.</span> 
